b88ff823d92b3e4e9f248662242e5280e09dfb61
shubham kaushik <shubhamkhaushik@gmail.com>
2024-07-30 09:35:07 +0530

    changes

--- route/connectionService.js
+++ route/connectionService.js
@@ -6,13 +6,7 @@ const router = express.Router();
 router.use(cookieParser())
 
 
-// you can use redis for storing the no of count of follower and follwing of each user 
-// user:{
-//     [656754cdtc78drcdcdc]:{
-//         followers:221,
-//         following:3123
-//     }
-// }
+
 router.post('/follow-user', middleware,followUser)
 router.get('/get-user-follower', middleware,getUserFollower)
 router.get('/get-user-following', middleware, getUserFollowing)
